02ea80c4b09e88e48bfa0547c68e1aa4adef193c
subakpup <hjsu0825@naver.com>
2025-11-21 12:00:16 +0900

    [Create] project init

---
+++ bang2bwa/src/main/java/com/ssafy/bbb/Bang2bwaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bang2bwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bang2bwaApplication.class, args);
+	}
+
+}
