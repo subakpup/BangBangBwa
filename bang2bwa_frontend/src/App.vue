888e547bd570d11f605b536afbd487d01e340e44
subakpup <hjsu0825@naver.com>
2025-11-26 16:23:36 +0900

    [Create] Vue.js init

---
+++ bang2bwa_frontend/src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">방방봐 홈</RouterLink>
+    </nav>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+/* 간단한 스타일 */
+header {
+  background-color: #f8f9fa;
+  padding: 20px;
+  text-align: center;
+  border-bottom: 1px solid #ddd;
+}
+nav a {
+  text-decoration: none;
+  color: #333;
+  margin: 0 10px;
+  font-weight: bold;
+}
+main {
+  padding: 20px;
+}
+</style>
---
+++ bang2bwa_frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+// 자바스크립트 로직 들어가는 곳
+</script>
+
+<template>
+    <div class="login-container">
+        <h2>로그인 페이지입니다</h2>
+        <p>주인님, 여기서 로그인을 구현할 예정입니다.</p>
+    </div>
+</template>
+
+<style scoped>
+/* CSS 들어가는 곳 */
+</style>
