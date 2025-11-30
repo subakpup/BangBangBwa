91ad2cd5627d83c009db9f678a48ed40d350b49c
Clarus23 <dnjs3023@gmail.com>
2025-11-30 18:38:22 +0900

    [Feat] Builder pattern 추가 (#6)

--- dummy-pg-server/src/main/java/com/ssafy/payment/dto/PaymentAuthDto.java
+++ dummy-pg-server/src/main/java/com/ssafy/payment/dto/PaymentAuthDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.payment.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,4 +10,11 @@ public class PaymentAuthDto {
 	private String orderId;
 	private Long amount;
 	private String type;
+
+	@Builder
+	public PaymentAuthDto(String orderId, Long amount, String type) {
+		this.orderId = orderId;
+		this.amount = amount;
+		this.type = type;
+	}
 }
--- dummy-pg-server/src/main/java/com/ssafy/payment/dto/PaymentCaptureDto.java
+++ dummy-pg-server/src/main/java/com/ssafy/payment/dto/PaymentCaptureDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.payment.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,4 +9,10 @@ import lombok.NoArgsConstructor;
 public class PaymentCaptureDto {
 	private String paymentKey;
 	private Long captureAmount;
+
+	@Builder
+	public PaymentCaptureDto(String paymentKey, Long captureAmount) {
+		this.paymentKey = paymentKey;
+		this.captureAmount = captureAmount;
+	}
 }
