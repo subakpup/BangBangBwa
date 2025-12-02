9537493f5cbafdf669b3d4054425391c85d73e94
Clarus23 <dnjs3023@gmail.com>
2025-12-02 17:47:17 +0900

    [Feat] OpenFeign 의존성 주입

--- bang2bwa/src/main/java/com/ssafy/bbb/Bang2bwaApplication.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/Bang2bwaApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.bbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Bang2bwaApplication {
 
 	public static void main(String[] args) {
