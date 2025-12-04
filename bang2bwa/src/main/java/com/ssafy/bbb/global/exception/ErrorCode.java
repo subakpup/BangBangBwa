13128e2a39e6534ca79a58f7a38a6b5880c3c869
Clarus23 <dnjs3023@gmail.com>
2025-12-04 22:26:52 +0900

    [Feat] 에러코드 추가 (#16)

--- bang2bwa/src/main/java/com/ssafy/bbb/global/exception/ErrorCode.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/global/exception/ErrorCode.java
@@ -25,7 +25,10 @@ public enum ErrorCode {
 	FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드 중 오류가 발생하였습니다."),
 	FILE_DELETE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제 중 오류가 발생하였습니다."),
 
-	// 5. 결제 관련 에러
+	// 5. 결제, 예약 관련 에러
+	RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 예약정보입니다."),
+	RESERVATION_UNAUTORIZATION(HttpStatus.FORBIDDEN, "자신의 매물만 승인가능합니다."),
+	RESERVATION_NOT_PENDING(HttpStatus.CONFLICT, "예약 대기중인 매물이 아닙니다."),
 	PAYMENT_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "결제가 실패하였습니다.");
 
 	// 필요한 에러 추가..
