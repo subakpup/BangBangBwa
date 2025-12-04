7285419c7060bb53c48fffb47627bc13b6fdc732
Clarus23 <dnjs3023@gmail.com>
2025-12-04 22:27:55 +0900

    [Feat] 부동산 업자의 매물 예약 승인 로직(repository) (#16)

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dao/ReservationDao.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dao/ReservationDao.java
@@ -1,8 +1,18 @@
 package com.ssafy.bbb.model.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.ssafy.bbb.model.dto.ReservationDto;
+import com.ssafy.bbb.model.enums.ReservationStatus;
 
 public interface ReservationDao {
+
+	public ReservationDto findById(Long reservationId);
+
 	// 예약자가 예약 요청
 	public Long reservationUser(ReservationDto reservation);
+
+	// 중개업자가 예약 수락
+	public void reservationAccept(@Param("reservationId") Long reservationId,
+			@Param("agentPaymentKey") String agentPaymentKey, @Param("status") ReservationStatus status);
 }
