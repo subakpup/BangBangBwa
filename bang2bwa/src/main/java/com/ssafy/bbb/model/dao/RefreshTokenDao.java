331098e9614d7000e59d5733a6a7309bc164352a
Clarus23 <dnjs3023@gmail.com>
2025-12-10 00:19:32 +0900

    [Comment] 주석 추가, 오타 수정 (#25)

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dao/RefreshTokenDao.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dao/RefreshTokenDao.java
@@ -4,12 +4,12 @@ import org.apache.ibatis.annotations.Param;
 
 public interface RefreshTokenDao {
 	
-	// 저장 (있다면 update, 있다면 insert)
+	// 토큰 저장 (있다면 update, 없다면 insert)
 	public void saveToken(@Param("userId") Long userId, @Param("refreshToken") String refreshToken);
 	
-	// 조회
+	// 토큰 조회
 	public String getToken(Long userId);
 	
-	// 삭제
+	// 토큰 삭제
 	public void deleteToken(Long userId);
 }
