26d32b10ca0263ae149aaf7a127e4eb18e213624
Clarus23 <dnjs3023@gmail.com>
2025-12-09 15:44:43 +0900

    [Feat] DTO에 API 문서 양식(예시 추가) (#23)

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dto/LocationDto.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/LocationDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.bbb.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LocationDto {
+	@Schema(description = "사용자의 현재 위치(위도)", example = "37.5642135")
 	private Double latitude; // 위도
+	@Schema(description = "사용자의 현재 위치(경도)", example = "127.0016985")
 	private Double longitude; // 경도
 }
--- bang2bwa/src/main/java/com/ssafy/bbb/model/dto/ReservationRequestDto.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/ReservationRequestDto.java
@@ -2,6 +2,7 @@ package com.ssafy.bbb.model.dto;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class ReservationRequestDto {
+	@Schema(description = "예약할 매물의 ID", example = "14")
 	private Long productId;
+	@Schema(description = "방문 예약 일시", example = "2025-12-26T14:00:00")
 	private LocalDateTime visitDate;
+	@Schema(description = "중개업자에게 전달할 메세지 (optional)", example = "오후 2시에 방문하겠습니다.")
 	private String message;
 }
