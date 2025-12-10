c6a482b77fb9f8a1260f5dd985b782ceaad6ae45
Clarus23 <dnjs3023@gmail.com>
2025-12-10 22:01:12 +0900

    [Feat] 회원 정보 조회, 수정, 비밀번호 변경을 위한 DTO 추가 (#25)

---
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/user/PasswordUpdateDto.java
@@ -0,0 +1,15 @@
+package com.ssafy.bbb.model.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateDto {
+	private String currentPassword;
+	private String newPassword;
+}
---
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/user/UserInfoDto.java
@@ -0,0 +1,29 @@
+package com.ssafy.bbb.model.dto.user;
+
+import java.time.LocalDate;
+
+import com.ssafy.bbb.model.enums.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 내 정보 조회용 DTO
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfoDto {
+	private Long userId;
+	private String email;
+	private String name;
+	private String phone;
+	private LocalDate birth;
+	private Role role;
+	
+	// Agent 추가 필드
+	private String ceoName;
+	private String realtorAgency;
+	private String businessNumber;
+}
