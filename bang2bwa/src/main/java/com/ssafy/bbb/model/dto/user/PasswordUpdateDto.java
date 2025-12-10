8e16001a3b61cca9a8a53fabe5d8edea49dbda8a
Clarus23 <dnjs3023@gmail.com>
2025-12-10 22:42:38 +0900

    [Feat] setter 추가 (#25)

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dto/user/PasswordUpdateDto.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/user/PasswordUpdateDto.java
@@ -4,9 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
-@Getter
+@Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PasswordUpdateDto {
