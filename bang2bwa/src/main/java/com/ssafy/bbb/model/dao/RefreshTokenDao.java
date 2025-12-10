647c75a54c23d7f56f2060e8755159b3d077a3b5
Clarus23 <dnjs3023@gmail.com>
2025-12-10 16:32:59 +0900

    [Feat] 리프레시 토큰 관련 디비 스키마 수정(Long id -> String email) (#25)

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dao/RefreshTokenDao.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dao/RefreshTokenDao.java
@@ -5,11 +5,11 @@ import org.apache.ibatis.annotations.Param;
 public interface RefreshTokenDao {
 	
 	// 토큰 저장 (있다면 update, 없다면 insert)
-	public void saveToken(@Param("userId") Long userId, @Param("refreshToken") String refreshToken);
+	public void saveToken(@Param("userEmail") String userEmail, @Param("refreshToken") String refreshToken);
 	
 	// 토큰 조회
-	public String getToken(Long userId);
+	public String getToken(String userEmail);
 	
 	// 토큰 삭제
-	public void deleteToken(Long userId);
+	public void deleteToken(String userEmail);
 }
