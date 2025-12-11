63c6c097f5bcf8583137d1e9581b10248da69c2d
subakpup <129526142+subakpup@users.noreply.github.com>
2025-12-11 17:37:10 +0900

    [Feat] API 호출 후 DB에 저장할 DTO 생성

---
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/HouseDealDto.java
@@ -0,0 +1,24 @@
+package com.ssafy.bbb.model.dto;
+
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HouseDealDto {
+	private @NonNull Long houseDealId; // 거래 ID(PK)
+	private @NonNull Long houseInfoId; // 매물 ID(FK)
+	private @NonNull Long dealAmount; // 거래 가격
+	
+	private LocalDate dealDate; // 거래일자
+	private String floor; // 층
+	private Double excluUseAr; // 전용 면적
+}
