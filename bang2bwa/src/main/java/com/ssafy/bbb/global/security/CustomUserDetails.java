7cd63eb6a2ec7abcf3aab688a552a6ef12932357
subakpup <hjsu0825@naver.com>
2025-12-19 16:15:10 +0900

    [Feat] 토큰 정보에 이름 정보 추가

--- bang2bwa/src/main/java/com/ssafy/bbb/global/security/CustomUserDetails.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/global/security/CustomUserDetails.java
@@ -14,18 +14,21 @@ import lombok.Getter;
 @Getter
 public class CustomUserDetails extends User {
 	private final Long userId;
-	
+	private final String name;
+
 	// 로그인 시 DB에서 가져온 UserDto로 생성하는 생성자
 	public CustomUserDetails(UserDto userDto) {
 		super(userDto.getEmail(), userDto.getPassword(),
 				Collections.singleton(new SimpleGrantedAuthority(userDto.getRole().toString())));
-		
+
 		this.userId = userDto.getUserId();
+		this.name = userDto.getName();
 	}
-	
+
 	// 토큰 파싱 후 Authentication 객체를 만들 때 사용하는 생성자 (이미 인증된 상태)
-	public CustomUserDetails(Long userId, String email, List<GrantedAuthority> authorities) {
+	public CustomUserDetails(Long userId, String email, String name, List<GrantedAuthority> authorities) {
 		super(email, "", authorities);
-        this.userId = userId;
+		this.userId = userId;
+		this.name = name;
 	}
 }
