30e948a33cc84f050cf040cf25d059c7f242b61a
Clarus23 <dnjs3023@gmail.com>
2025-12-19 15:37:35 +0900

    [Refactor] 회원정보 조회, 수정, 삭제, 비밀번호 변경 Controller 분리 (#29)

---
+++ bang2bwa/src/main/java/com/ssafy/bbb/controller/docs/MyPageControllerDocs.java
@@ -0,0 +1,23 @@
+package com.ssafy.bbb.controller.docs;
+
+import com.ssafy.bbb.global.response.ApiResponse;
+import com.ssafy.bbb.global.security.CustomUserDetails;
+import com.ssafy.bbb.model.dto.user.PasswordUpdateDto;
+import com.ssafy.bbb.model.dto.user.UserInfoDto;
+import com.ssafy.bbb.model.dto.user.UserUpdateDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+public interface MyPageControllerDocs {
+    @Operation(summary = "내 정보 조회", description = "현재 로그인한 사용자의 정보를 조회합니다.")
+    public ApiResponse<UserInfoDto> getUserInfo(CustomUserDetails user);
+
+    @Operation(summary = "내 정보 수정", description = "이름, 전화번호, 생년월일 등을 수정합니다.")
+    public ApiResponse<String> updateUserInfo(CustomUserDetails user, UserUpdateDto request);
+    
+    @Operation(summary = "비밀번호 수정", description = "비밀번호를 수정합니다.")
+    public ApiResponse<String> updatePassword(CustomUserDetails user, PasswordUpdateDto newPassword);
+
+    @Operation(summary = "회원 탈퇴", description = "회원 정보를 삭제합니다.")
+    public ApiResponse<String> withdraw(CustomUserDetails user);
+}
--- bang2bwa/src/main/java/com/ssafy/bbb/controller/docs/UserControllerDocs.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/controller/docs/UserControllerDocs.java
@@ -7,10 +7,7 @@ import com.ssafy.bbb.global.response.ApiResponse;
 import com.ssafy.bbb.global.security.CustomUserDetails;
 import com.ssafy.bbb.model.dto.TokenInfo;
 import com.ssafy.bbb.model.dto.user.LoginRequestDto;
-import com.ssafy.bbb.model.dto.user.PasswordUpdateDto;
 import com.ssafy.bbb.model.dto.user.SignupRequestDto;
-import com.ssafy.bbb.model.dto.user.UserInfoDto;
-import com.ssafy.bbb.model.dto.user.UserUpdateDto;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -57,18 +54,6 @@ public interface UserControllerDocs {
 	@Operation(summary = "로그아웃", description = "서버에서 Refresh Token을 삭제합니다.")
     public ApiResponse<String> logout(CustomUserDetails user);
 
-    @Operation(summary = "내 정보 조회", description = "현재 로그인한 사용자의 정보를 조회합니다.")
-    public ApiResponse<UserInfoDto> getUserInfo(CustomUserDetails user);
-
-    @Operation(summary = "내 정보 수정", description = "이름, 전화번호, 생년월일 등을 수정합니다.")
-    public ApiResponse<String> updateUserInfo(CustomUserDetails user, UserUpdateDto request);
-    
-    @Operation(summary = "비밀번호 수정", description = "비밀번호를 수정합니다.")
-    public ApiResponse<String> updatePassword(CustomUserDetails user, PasswordUpdateDto newPassword);
-
-    @Operation(summary = "회원 탈퇴", description = "회원 정보를 삭제합니다.")
-    public ApiResponse<String> withdraw(CustomUserDetails user);
-    
     @Operation(summary = "이메일 인증 코드 발송", description = "이메일로 6자리 인증 코드를 전송합니다.")
     public ApiResponse<String> sendEmailVerification(String email);
     
