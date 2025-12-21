d87486162ff59d077e334ae91165cf593d9ff87f
subakpup <129526142+subakpup@users.noreply.github.com>
2025-12-22 01:25:57 +0900

    [Feat] 라우터 추가

--- bang2bwa_frontend/src/router/index.js
+++ bang2bwa_frontend/src/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import SignupView from '@/views/user/SignupView.vue'
 import LoginView from '@/views/user/LoginView.vue'
+import ProductManageView from '@/views/product/ProductManageView.vue'
+import ProductFormView from '@/views/product/ProductFormView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +23,21 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView
+    },
+    {
+      path: '/product/manage',
+      name: 'productManage',
+      component: ProductManageView
+    },
+    {
+      path: '/product/register',
+      name: 'productRegister',
+      component: ProductFormView
+    },
+    {
+      path: '/product/edit/:id',
+      name: 'productEdit',
+      component: ProductFormView
     },
   ]
 })
