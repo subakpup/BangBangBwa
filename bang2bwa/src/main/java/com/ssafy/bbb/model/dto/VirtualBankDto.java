598956d6b02a7c2bf719eac7dda98dd59cdda5b2
Clarus23 <dnjs3023@gmail.com>
2025-12-23 01:30:43 +0900

    [Feat] 사용자가 결제할 수단을 선택할 수 있도록 결제 수단을 저장할 스키마 생성 (#52)

---
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/VirtualBankDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.bbb.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class VirtualBankDto {
+    private Long bankId;       // 은행 PK
+    private String bankName;   // 은행 이름
+    private String webhookUrl; // 웹훅 URL (중요: 프론트로 내려줄 땐 제외하는 것이 좋음)
+}
