b60b562bba7ac245ad31f6e946aefe3c41ddc350
subakpup <129526142+subakpup@users.noreply.github.com>
2025-12-23 01:37:05 +0900

    [Feat] AI 추천 시스템을 위한 DTO 구성

--- bang2bwa/src/main/java/com/ssafy/bbb/model/dto/AiSearchDto.java
+++ bang2bwa/src/main/java/com/ssafy/bbb/model/dto/AiSearchDto.java
@@ -11,18 +11,18 @@ public class AiSearchDto {
 	
 	@Data
 	public static class Budget {
-		private Integer min; // 최소
-		private Integer max; // 최대
+		private Long min; // 최소
+		private Long max; // 최대
 	}
 	
 	private TradeType tradeType;	// 거래 유형(월세, 전세, 매매)
 	
 	// 월세, 전세
 	private Budget deposit;	// 보증금
-	private Budget monthly;	// 월세
+	private Budget monthlyRent;	// 월세
 	
 	// 매매
-	private Budget budget;	// 총 예산
+	private Budget dealAmount;	// 총 예산
 	
 	private String location; // 희망 지역
 	private List<String> options; // 선호 옵션
